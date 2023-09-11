d26339e3eb34f018c4fe1be485e7cf8917747f20
hosenur <nur08439@gmail.com>
2023-09-12 02:07:53 +0530

    Created Schemas, and created basic routes for project

--- app/routes/Projects.js
+++ app/routes/Projects.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import prismaClient from "../utils/prisma.js";
 const router = Router()
-router.get("/", (req, res) => {
-    res.json([
-        { id: 1, name: "Project 1" },
-    ]).status(200)
+router.get("/", async (req, res) => {
+    const projects = await prismaClient.projects.findMany()
+    res.json(projects).status(200)
+})
+router.get("/add", async (req, res) => {
+    const project = await prismaClient.projects.create({
+        data: {
+
+            name: "Sample Project"
+        }
+    })
+    res.json(project).status(201)
 })
 export default router;
