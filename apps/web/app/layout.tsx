d4cdac31ec51147737f798e9b96018dfb19ef905
hosenur <mail@hosenur.dev>
2024-04-12 12:54:54 +0530

    init: create readme

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-black">{children}</body>
     </html>
   );
 }
